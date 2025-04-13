d58a6af4ccfe8ed9b817297c671e14b757edfdd7
Kevin W. McConeghy <kevinmcconeghy@gmail.com>
2025-04-13 14:39:20 -0400

    setup

---
+++ src/onetime.R
@@ -0,0 +1,5 @@
+# To be run once at project start
+  renv::init()
+  install.packages('remotes')
+  install.packages(c('tidyverse', 'lubridate', 'here', 'knitr', 'quarto',
+                     'future', 'progressr'))
